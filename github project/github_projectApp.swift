2030064029f4766d8aa4a00496cdb9708a1cb354
Siena <sienajoshi@gmail.com>
2023-06-26 15:29:28 -0400

    Initial Commit

---
+++ github project/github_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_projectApp.swift
+//  github project
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct github_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
